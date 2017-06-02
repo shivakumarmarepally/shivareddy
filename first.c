aac14fb3067fce931a4fdb527c4ad7908cfd7ddf
Shiva reddy <shivakumar.marepally@spoors.in>
2017-06-02 13:09:13 +0530

    changes made

--- first.c
+++ first.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 void main()
 {
-<<<<<<< HEAD
 printf("hello insta");
 printf("hello whatsapp");
-=======
 printf("hello friend");
 printf("hello gmail");
->>>>>>> master
 printf("hello facebook");
 printf("hello google");
 printf("hello world");
